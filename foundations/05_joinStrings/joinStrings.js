8ce35c60df6615d1121f6b2e66456be42bd18e6c
mattjaredmamaril <tytjared@gmail.com>
2026-04-01 21:12:33 -0500

    feat(joinStrings): add greeting message with name and age calculation

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -8,6 +8,12 @@
 */
 
 // Add your code right below, good luck!
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+const age = thisYear - birthYear;
+const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + age + " years old.";
 
 
 
